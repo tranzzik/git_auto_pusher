bf476da711371a5d45960483894685f0da5f5b3c
tranzzik <stasiakjulian@gmail.com>
2022-10-24 21:20:15 +0200

    Auto commit 2022-10-24

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from git import Repo
 from datetime import datetime
-import random
 
 PATH_OF_GIT_REPO = r'C://programming//python//git_auto_pusher//.git'  # make sure .git folder is properly configured
 TODAY = datetime.today().strftime('%Y-%m-%d')
+TODAY_FULL = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 COMMIT_MESSAGE = f'Auto commit {TODAY}'
 
 def git_push():
@@ -17,6 +17,6 @@ def git_push():
         print('Some error occured while pushing the code')
 
 with open("C://programming//python//git_auto_pusher//date.txt", "w") as f:
-    f.write(str(random.randrange(0,9_999_999_999)))
+    f.write(TODAY_FULL)
 
 git_push()
