847ed7cf413e93fa2aa1a4f5a0dcc81dcf411e57
Julian Stasiak <stasiakjulian@gmail.com>
2023-03-08 12:58:41 +0100

    Auto commit 2023-03-08

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from git import Repo
 from datetime import datetime
+import platform
+import os
 
-PATH_OF_GIT_REPO = r'C://programming//python//git_auto_pusher//.git'  # make sure .git folder is properly configured
+
 TODAY = datetime.today().strftime('%Y-%m-%d')
 TODAY_FULL = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 COMMIT_MESSAGE = f'Auto commit {TODAY}'
+
+if platform.system() == 'Linux':
+    PATH_OF_GIT_REPO = os.getcwd()  
+    with open("date.txt", "w") as f:
+        f.write(TODAY)
+else:
+    PATH_OF_GIT_REPO = r'C://programming//python//git_auto_pusher//.git'  
+    with open("C://programming//python//git_auto_pusher//date.txt", "w") as f:
+        f.write(TODAY)
 
 def git_push():
     try:
@@ -16,7 +27,5 @@ def git_push():
     except:
         print('Some error occured while pushing the code')
 
-with open("C://programming//python//git_auto_pusher//date.txt", "w") as f:
-    f.write(TODAY)
 
 git_push()
